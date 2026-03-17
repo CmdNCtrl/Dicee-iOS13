1d8292a5766a335c131ad0c227262393b48a235c
jdruding <40360443+CmdNCtrl@users.noreply.github.com>
2026-03-16 22:30:07 -0400

    Start of Dicee app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Ctrl Drage the image to here so you can reference it.
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Set an initial dice image from your asset catalog (e.g., "DiceOne")
+        // Set the dice image and the alpha value
+        //The icon itself is a "image literal".  Create by typing: #imageLiteral(
+        //Don't forget the capital "L"
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView1.alpha = 0.25
+        
+        // Set dice2
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
+        
     }
 
 
